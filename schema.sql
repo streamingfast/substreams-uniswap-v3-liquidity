f7437ac66e37b51417bb9208399736a3a24d6651
Matthieu Vachon <matt@streamingfast.io>
2023-06-22 16:45:53 -0400

    Updated schema to be by block

--- schema.sql
+++ schema.sql
@@ -1,18 +1,23 @@
-create table pool_liquidity
+create table pool
 (
     address     text not null constraint address_pk primary key,
-    liquidity   decimal,
-    current_tick integer,
     token0_address text,
     token0_symbol text,
     token0_decimals integer,
     token1_address text,
     token1_symbol text,
     token1_decimals integer,
+);
+
+create table liquidity {
+    pool_address text,
+    block_number integer,
+    liquidity   decimal,
+    current_tick integer,
     tick_lower_tick_idx integer,
     tick_upper_tick_idx integer,
     tick_liquidity decimal,
-);
+}
 
 create table cursors
 (
